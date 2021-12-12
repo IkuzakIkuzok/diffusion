cdc1670caf14a333116eac5aeff927fdff3846ed
ikuzak <k.kohzuki@gmail.com>
2021-12-12 14:51:36 +0900

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,97 @@
+
+import click
+import pandas as pd
+from cv2 import destroyAllWindows
+from os.path import splitext
+from particle_utils import ParticlesData
+
+
+class Displacement():
+    '''Represents a displacement vector.
+    '''
+
+    def __init__(self, datum):
+        '''Initializes a new instance of the `Displacement` class.
+
+        Args:
+            datum: A datum, which contains x- and y-coordinates
+                in actual size, and the coordinate.
+
+        Attributes:
+            x (int): x-coordinate in actual size.
+            y (int): y-coordinate in actual size.
+            coordinate: The coordinate on an image.
+        '''
+        self.x, self.y, self.coordinate = datum
+
+
+class Displacements(list):
+    '''Represents the change in displacement vector over time.
+    '''
+
+    def __init__(self, data):
+        '''Initializes a new instance of the `Displacements` class.
+
+        Args:
+            data: a list of tuples of x- and y-coordinates
+                in actual size, and coordinates.
+        '''
+        super().__init__(map(Displacement, data))
+
+    @property
+    def X(self):
+        '''Change in x-coordinates over time.
+        '''
+        t = -10
+        return {(t:= t+10): vec.x for vec in self}
+
+    @property
+    def Y(self):
+        '''Change in y-coordinates over time.
+        '''
+        t = -10
+        return {(t:= t+10): vec.y for vec in self}
+
+    @property
+    def Xsq(self):
+        '''Variation of squared displacement in x-direction with time.
+        '''
+        return {t: x**2 for t, x in self.X.items()}
+
+    @property
+    def Ysq(self):
+        '''Variation of squared displacement in y-direction with time.
+        '''
+        return {t: y**2 for t, y in self.Y.items()}
+
+
+@click.command()
+@click.argument('source')
+@click.option('--destination', '-o', default=None)
+@click.option('--mode', '-m', default='w')
+@click.option('--threshold', '-t', default=150)
+@click.option('--width', '-w', default=150)
+@click.option('--height', '-h', default=150)
+@click.option('--distance', '-d', default=15)
+@click.option('--lower', '-l', default=2)
+def main(source, destination, mode, threshold, width, height, distance, lower):
+    fname = splitext(source)[0]
+    if destination is None:
+        destination = fname + ".csv"
+
+    data = ParticlesData(source, threshold, width, height, distance)
+    displacements = map(Displacements, data.displacements(lower))
+    dfm = pd.DataFrame()
+    for displacement in displacements:
+        dfm = dfm.append(
+            [displacement.Xsq, displacement.Ysq], ignore_index=True
+        )
+    dfm.to_csv(destination, mode=mode, header=False, index=False)
+    #data.mark_particles()
+    #data.show()
+
+if __name__ == '__main__':
+    try:
+        main()
+    finally:
+        destroyAllWindows()
---
+++ particle_utils.py
@@ -0,0 +1,281 @@
+
+import cv2
+import numpy as np
+from functools import partial
+
+
+def to_list(func):
+    '''Converts the return value of the generator to a list.
+    '''
+    def __wrapper(*args, **kwargs):
+        return list(func(*args, **kwargs))
+    return __wrapper
+
+
+class Particle():
+    '''Represents an individual particle.
+
+    This class can also be used to represent the change in coordinates
+    of particles over time as a linear list.
+
+    Attributes:
+        x (int): x-coordinate of the particle.
+        y (int): y-coordinate of the particle.
+    '''
+
+    def __init__(self, contour, size_coeffs):
+        '''Initializes a new instance of the `Particle` class.
+
+        Args:
+            contour: Coordinates of the particle contour.
+            One of the return values of `cv2.findContours` is used as is.
+            size_coeffs: Coefficients for converting the coordinates
+                of a particle to its actual distance.
+        '''
+        X = [pt[0][0] for pt in contour]
+        Y = [pt[0][1] for pt in contour]
+        self.x, self.y = round(np.average(X)), round(np.average(Y))
+        self.__prev = None
+        self.__next = None
+        self.size_coeffs = size_coeffs
+
+    def __repr__(self):
+        return f'({self.x}, {self.y})'
+
+    def __sub__(self, other):
+        return self.__class__(
+            [[[self.x - other.x, self.y - other.y]]], self.size_coeffs
+        )
+
+    def distance_to(self, other):
+        '''Calculates the distance to another particle.
+
+        Args:
+            other: The other particle.
+
+        Returns:
+            The distance.
+        '''
+        return np.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
+
+    @property
+    def coordinate(self):
+        '''Tuple of x- and y-coordinates of the particle.
+        '''
+        return self.x, self.y
+
+    def find_nearest(self, particles, distance):
+        '''Finds the particle that is closest to the current particle.
+
+        Args:
+            particles: A `Particles` object
+                from which the nearest one is looked for.
+            distance: The longest distance to search for a particle.
+
+        Returns:
+            If there are particles within `distance`, the closest particle;
+            otherwise, `None`.
+        '''
+        p = min(particles, key=lambda p: self.distance_to(p))
+        return p if self.distance_to(p) < distance else None
+
+    @property
+    def prev(self):
+        '''A particle that represents the position of the current particle
+        at a previous time.'''
+        return self.__prev
+
+    @prev.setter
+    def prev(self, value):
+        self.__prev = value
+        if value:
+            value.__next = self
+
+    @property
+    def next(self):
+        '''A particle that represents the position of the current particle
+        at a next time.'''
+        return self.__next
+
+    @next.setter
+    def next(self, value):
+        self.__next = value
+        if value:
+            value.__prev = self
+
+    @property
+    def displacements(self):
+        '''Future displacement vector starting from the current particle.
+
+        Note:
+            Refering this property may destroy linear list struct.
+        '''
+        O = self
+        _x, _y = self.size_coeffs
+        ret = [(0, 0, self.coordinate)]
+        p = self
+        while p.next:
+            p = p.next
+            p.prev.next = None
+            x, y = (p - self).coordinate
+            ret.append((x * _x, y * _y, p.coordinate))
+        return ret
+
+
+class Particles(list):
+    '''Represents a group of particles at a specific time.
+    '''
+
+    def __init__(self, contours, size_coeffs):
+        '''Initializes a new instance of the `Particles` class.
+
+        Args:
+            contours: Coordinates of the contour of each particle.
+            size_coeffs: Coefficients for converting the coordinates
+                of a particle to its actual distance.
+        '''
+        super().__init__(
+            map(partial(Particle, size_coeffs=size_coeffs), contours)
+        )
+
+    def link_next(self, particles, distance):
+        '''Links the group of particles at the next time with the current one.
+
+        Note:
+            This method is used to track changes in particles over time.
+        '''
+        for particle in self:
+            p = particle.find_nearest(particles, distance)
+            particle.next = p
+
+    @to_list
+    def displacements(self, lower_limit):
+        '''Detects displacement vectors
+        starting at the current time for each particle.
+
+        Note:
+            Calling this method may destroy linear list struct.
+
+        Args:
+            lower_limit: Lower limit of the number of tracks over time.
+
+        Yields:
+            list: A list of displacement vectors.
+        '''
+        for particle in self:
+            d = particle.displacements
+            if len(d) < lower_limit:
+                continue
+            yield d
+
+
+class ParticlesData(list):
+    '''Represents the information for each particle at all times.
+
+    Attributes:
+        path (str): Path of the gif image.
+        gif (list): List of frames corresponding to each time.
+        size_coeffs: Coefficients for converting the coordinates
+            of a particle to its actual distance.
+    '''
+
+    def __init__(self, path, threshold, width, height, distance):
+        '''Initializes a new instance of the `ParticlesData` class.
+
+        Args:
+            path: Path of the gif image from which data is loaded.
+            threshold: Threshold value used to binarize an image.
+            width: The actual size of the image's width.
+            height: The actual size of the image's height.
+            distance: The longest distance to search for a particle.
+        '''
+        self.gif = self.load_gif(path, threshold)
+        w, h = self.gif[0].shape
+        if not width:
+            width = w
+        if not height:
+            height = h
+        self.size_coeffs = width / w, height / h
+
+        particles = map(self.get_particles, self.gif)
+        super().__init__(particles)
+
+        for i, particles in enumerate(self[:-1]):
+            particles.link_next(self[i+1], distance)
+
+    def load_gif(self, path, threshold):
+        '''Loads a gif image.
+
+        Args:
+            path: Path of the gif image from which data is loaded.
+            threshold: Threshold value used to binarize an image.
+
+        Returns:
+            Loaded gif image.
+            The image is a black and white image binarized
+            by a `threshold` value.
+        '''
+        self.path = path
+        cap = cv2.VideoCapture(path)
+        gif = []
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                org = cv2.bitwise_not(frame)
+                gray = cv2.cvtColor(org, cv2.COLOR_BGR2GRAY)
+                th, im = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
+                gif.append(im)
+            else:
+                cap.release()
+
+        return gif
+
+    def get_particles(self, frame):
+        '''Detects particle contours from a frame.
+
+        Args:
+            frame: The frame from which particles are detected.
+
+        Returns:
+            A `Particles` object containing detected particles.
+        '''
+        contours, hierarchy = cv2.findContours(
+            frame, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE
+        )
+        # exclude the end of the list
+        # because the entire frame is also detected as a contour
+        return Particles(contours[:-1], self.size_coeffs)
+
+    def mark_particles(self):
+        '''Marks the positions of the particles on the gif image.
+        '''
+        for frame, coordinates in zip(self.gif, self):
+            for particle in coordinates:
+                frame = cv2.circle(
+                    frame, particle.coordinate, radius=10, color=(0, 0, 0)
+                )
+
+    def show(self, interval=100):
+        '''Shows a gif image.
+
+        Args:
+            interval (int, optional): The interval between each frame
+                in milliseconds.
+        '''
+        for frame in self.gif:
+            cv2.imshow(self.path, frame)
+            cv2.waitKey(interval)
+
+    def displacements(self, lower_limit):
+        '''Detects all detectable displacement vectors.
+
+        Note:
+            Calling this method may destroy linear list struct.
+
+        Args:
+            lower_limit: Lower limit of the number of tracks over time.
+
+        Returns:
+            A list of displacement vectors.
+        '''
+        return sum((l.displacements(lower_limit) for l in self), [])
